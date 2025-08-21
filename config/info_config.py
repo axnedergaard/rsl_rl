2c33ab18d16f4f118acec4e99e93b5088a607ecf
Alexander Nedergaard <axnedergaard@gmail.com>
2025-08-21 13:51:15 +0200

    Add attempt at fixing config

--- config/info_config.py
+++ config/info_config.py
@@ -20,3 +20,19 @@ class RewarderCfg:
   scaling = 1.0,
   density = DensityCfg(),
   info_geom_cfg = InformationGeometryCfg(),
+
+@configclass
+class InfoAlgorithmCfg:
+  value_loss_coef = 1.0,
+  use_clipped_value_loss = True,
+  clip_param = 0.2,
+  entropy_coef = 0.01,
+  num_learning_epochs = 5,
+  num_mini_batches = 4,
+  learning_rate = 1.0e-3,
+  schedule = "adaptive",
+  gamma = 0.99,
+  lam = 0.95,
+  desired_kl = 0.01,
+  max_grad_norm = 1.0,
+  rewarder=RewarderCfg(),
